896166a4835beff8a3bd4aebf2e09d668fb5799e
Moez Hachicha <moez.hachicha@icloud.com>
2025-11-30 20:36:08 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://moezh.com",
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
